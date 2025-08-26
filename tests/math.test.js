ee9cb7ec8fb56429a63900118555d99baf161641
Anderson Mazini <andersonsmazini@gmail.com>
2025-08-26 00:48:41 +0000

    Finalizado comunicação

---
+++ tests/math.test.js
@@ -0,0 +1,20 @@
+const math = require('../math');
+test('soma 2 + 3 igual a 5', () => { 
+expect(math.add(2, 3)).toBe(5); 
+}); 
+
+test('Subtracao 3 - 2 igual a 1', () => { 
+expect(math.subtract(3, 2)).toBe(1); 
+}); 
+
+test('Multiplicacao 2 3 igual a 6', () => { 
+expect(math.multiply(2, 3)).toBe(6); 
+}); 
+
+test('Divisao 4 / 2 igual a 2', () => { 
+expect(math.divide(4, 2)).toBe(2); 
+}); 
+
+
+
+
